0521facac04d455599bf13a24802d6b85865686f
Bleh-Blah <haikalirfano@gmail.com>
2022-03-03 12:16:05 +0700

    Empty activity for main chat

--- app/src/main/java/com/example/mypsikolog/MenuActivity.kt
+++ app/src/main/java/com/example/mypsikolog/MenuActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mypsikolog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        
+        btnChat.setOnClickListener {
+            val intent = Intent(this, MainChatActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
